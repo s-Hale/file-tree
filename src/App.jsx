c6149eaefa326f5c0766d81e24965faa65ad81cc
Sally Hale <sally.hale@laterooms.com>
2019-08-08 18:20:32 +0100

    feat: filter function, minor bugs with date and sort

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ class App extends React.Component {
 
   render() {
     const sortByName = () => {
-      let result = this.state.originalData.sort((a, b) => {
+      const tempState = this.state.originalData.slice();
+      let result = tempState.sort((a, b) => {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
         return 0;
@@ -23,16 +24,21 @@ class App extends React.Component {
     };
 
     const sortByDate = () => {
-      let result = this.state.originalData.sort(
-        (a, b) => new Date(b.added) - new Date(a.added)
-      );
+      const tempState = this.state.originalData.slice();
+      let result = tempState.sort((a, b) => {
+        if (!a.added && !b.added) return 0;
+        if (a.added && !b.added) return -1;
+        if (!a.added && b.added) return 1;
+        return new Date(b.added) - new Date(a.added);
+      });
       this.setState({
         updatedData: result
       });
     };
 
     const sortByType = () => {
-      let result = this.state.originalData.sort((a, b) => {
+      const tempState = this.state.originalData.slice();
+      let result = tempState.sort((a, b) => {
         if (a.type !== "folder") return 1;
         else return -1;
       });
@@ -41,7 +47,16 @@ class App extends React.Component {
       });
     };
 
-    const handleTypeSearch = () => null;
+    const handleTypeSearch = e => {
+      const tempState = this.state.originalData.slice();
+      let userEntry = e.target.value.toLowerCase();
+      let result = tempState.filter(each =>
+        each.name.toLowerCase().includes(userEntry)
+      );
+      this.setState({
+        updatedData: result
+      });
+    };
 
     return (
       <div className="wrapper-app">
